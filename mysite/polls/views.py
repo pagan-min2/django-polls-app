742fb7c3e9f2132a7eda73c3ee6dfd302b289118
Utsav Shukla <utsav.academics@gmail.com>
2026-09-21 08:30:56 +0530

    Add voting form, complete vote view, and build results template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,10 @@
-from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
-from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -12,12 +13,27 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
 def detail(request, question_id):
