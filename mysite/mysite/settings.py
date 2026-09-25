f04eb81bedefb329a1f0ca5759b97c2d50f1feaf
Utsav Shukla <utsav.academics@gmail.com>
2026-09-25 14:14:37 +0530

    Add Django Debug Toolbar

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,4 +130,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
